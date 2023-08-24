96e50e693d826b73d82f258187e902bffce21a2c
Juan Camilo Villacob <jucaviza6@gmail.com>
2023-08-24 17:32:43 -0500

    correo

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  firstName: string = '';
+  email: string = '';
+  subject: string = '';
+  message: string = '';
 
+  sendMessage(event: Event) {
+    event.preventDefault();
+    const body = `First Name: ${this.firstName}\nEmail: ${this.email}\nSubject: ${this.subject}\n\n${this.message}\n\n`;
+    window.location.href = `mailto:jucaviza6@gmail.com?subject=${encodeURIComponent(this.subject)}&body=${encodeURIComponent(body)}`;
+  }  
+  
 }
