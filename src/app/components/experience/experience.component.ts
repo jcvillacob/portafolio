3a52ede6f0c57f108d49c82ec5af70b2a24dbb09
jvillacob <jvillacob@coorditanques.com>
2024-03-05 10:41:05 -0500

    Experiencia corregida

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 import { ThemeService } from 'src/app/services/theme.service';
 
 @Component({
@@ -7,26 +6,22 @@ import { ThemeService } from 'src/app/services/theme.service';
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent implements OnDestroy {
-  language!: Observable<number>;
+export class ExperienceComponent {
   general: any = {};
   generals: any[] = [
-    {subtitle: 'Experience', title: 'MY EXPERIENCE', paragraph: 'My career path includes diverse roles such as teaching teenage students and managing a bar, reflecting my ability to adapt and lead in various environments.'},
-    {subtitle: 'Experiencia', title: 'MI EXPERIENCIA', paragraph: 'Mi trayectoria profesional incluye roles diversos como enseñar a estudiantes adolescentes y gestionar un bar, reflejando mi capacidad para adaptarme y liderar en varios entornos.'}
+    { subtitle: 'Experience', title: 'MY EXPERIENCE', paragraph: 'My career path includes diverse roles such as teaching teenage students and managing a bar, reflecting my ability to adapt and lead in various environments.' },
+    { subtitle: 'Experiencia', title: 'MI EXPERIENCIA', paragraph: 'Mi trayectoria profesional incluye roles diversos como enseñar a estudiantes adolescentes y gestionar un bar, reflejando mi capacidad para adaptarme y liderar en varios entornos.' }
   ];
-
-  private languageSubscription: Subscription;
+  lists: any[] = [
+    { fdate: '2024', fcat: 'Project Management and Web Development', fexp: 'PROJECT PROFESSIONAL', sdate: '2022', scat: 'Automation Analyst and Web Development', sexp: 'FULL STACK WEB DEVELOPER', tdate: '2021', tcat: 'Teacher for Teenage Students', texp: 'TEACHER' },
+    { fdate: '2024', fcat: 'Gestión de Proyectos y desarrollo web', fexp: 'PROFESIONAL DE PROYECTOS', sdate: '2022', scat: 'Desarrollo web y análisis de automatizaciones', sexp: 'DESARROLLADOR WEB FULL STACK', tdate: '2021', tcat: 'Profesor de estudiantes adolescentes', texp: 'PROFESOR PRE-ICFES' }
+  ]
+  list: any = {};
 
   constructor(private themeService: ThemeService) {
-    this.language = this.themeService.language$;
-    this.languageSubscription = this.language.subscribe(lang => {
+    this.themeService.language$.subscribe(lang => {
       this.general = this.generals[lang];
+      this.list = this.lists[lang];
     });
-  }
-
-  ngOnDestroy() {
-    if (this.languageSubscription) {
-      this.languageSubscription.unsubscribe();
-    }
   }
 }
